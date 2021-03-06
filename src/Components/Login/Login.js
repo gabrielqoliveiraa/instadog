177aa14daa064d8b1f41dbf040bec60fc11b20b4
Gabriel Queiroz <gabrielqoliveiraa@gmail.com>
2021-03-06 02:28:57 -0300

    Create AccountCreate Screen

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import LoginCreate from './LoginCreate'
 import LoginPasswordLost from './LoginPasswordLost'
 import LoginPasswordReset from './LoginPasswordReset'
 import { UserContext } from '../../UserContext'
+import styles from '../../styles/Components/Login.module.css'
 
 const Login = () => {
 
@@ -12,19 +13,23 @@ const Login = () => {
 
     if(login===true)return <Navigate to='/conta'/>
     return (
-        <div>
-            <Routes>
-                <Route path='/' element={<LoginForm />}/>
-                <Route path='/criar' element={<LoginCreate />}/>
-                <Route path='/perdeu' element={<LoginPasswordLost />}/>
-                <Route path='/resetar' element={<LoginPasswordReset />}/>
+        <section className={styles.login}>
+            <div lassName={styles.forms}>
+                <Routes>
+                    <Route path='/' element={<LoginForm />}/>
+                    <Route path='/criar' element={<LoginCreate />}/>
+                    <Route path='/perdeu' element={<LoginPasswordLost />}/>
+                    <Route path='/resetar' element={<LoginPasswordReset />}/>
 
 
 
+                </Routes>
+            </div>
+        </section>
 
-            </Routes>
-        </div>
+
     )
 }
+
 
 export default Login
--- src/UserContext.js
+++ src/UserContext.js
@@ -55,7 +55,6 @@ export const UserStorage = ({children}) => {
         const json = await response.json();
         setData(json)
         setLogin(true)
-        console.log(json);
     }
 
 
