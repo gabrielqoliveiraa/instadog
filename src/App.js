a861087b6cf66dd353803e6b013abef028f32cca
Gabriel Queiroz <gabrielqoliveiraa@gmail.com>
2021-03-06 01:18:49 -0300

    Validate Login and useContextLogin

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 
 
+import {UserStorage} from './UserContext'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import Home from './Components/Home'
 import Login from './Components/Login/Login'
+
 
 import './App.css'
 
@@ -16,16 +18,22 @@ const App = () => {
 
   return (
     <div>
-
+    
       <BrowserRouter>
-        <Header/>
-        <Routes>
-          <Route path='/' element={ <Home/>}/>
-          <Route path='/login/*' element={ <Login/>}/>
-        </Routes>
-        <Footer/>
-      
+
+        <UserStorage>
+          <Header/>
+          <Routes>
+            <Route path='/' element={ <Home/>}/>
+            <Route path='/login/*' element={ <Login/>}/>
+          </Routes>
+          <Footer/>
+        </UserStorage>
+        
+
       </BrowserRouter>
+
+
 
 
 
