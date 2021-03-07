716c08d392e56317cc155fb9d8e4da7eed23d10e
Gabriel Queiroz <gabrielqoliveiraa@gmail.com>
2021-03-07 15:48:26 -0300

    Create Page 404

---
+++ src/Components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='container'>
+            <h1 className='title'>Página não encontrada</h1>
+        </div>
+    )
+}
+
+export default NotFound
